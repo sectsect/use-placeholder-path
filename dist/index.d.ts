7b6c9617733be77c01c9a7f6e8ae83b8cf1176ae
sect <sect@sect.me>
2024-08-11 20:54:22 +0900

    chore: remove dist files from Git scope

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A custom React hook to retrieve placeholder path in Next.js App Router.
- * @returns The placeholder path with dynamic segments replaced by their parameter names.
- */
-declare const usePlaceholderPath: () => string;
-
-export { usePlaceholderPath as default };
